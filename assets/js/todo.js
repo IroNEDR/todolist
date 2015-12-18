d04d761a0fd15cf3f4d1dd1b8aba16a616592d69
IroNEDR <ekintiras@gmx.de>
2015-12-18 23:22:43 +0100

    function for crossing out dynamicly added todos, delete function and insert new todos with enter added

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,6 +1,22 @@
+/*jshint jquery: true*/
 /*jslint browser: true, devel: true, sloppy: true*/
 /*jshint browser: true, jquery: true*/
 //Check off todos
-$("li").click(function () {
+$("ul").on("click", "li", function () { //when a li is clicked inside a ul. necessary for dynamicly added elements same for delete function
     $(this).toggleClass("selected");
+});
+
+$("ul").on("click", "span", function () { //on click of a span within a ul, run this event
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) { // 13 ist keycode für Enter taste
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
