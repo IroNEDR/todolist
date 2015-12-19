9784496ea54e7b102ee7b20624e175d3282c4fba
IroNEDR <ekintiras@gmx.de>
2015-12-19 18:56:31 +0100

    Background changed, toggle function added, styling finished. Project is done

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -17,6 +17,9 @@ $("input[type='text']").keypress(function (event) {
     if (event.which === 13) { // 13 ist keycode für Enter taste
         var todoText = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
     }
+});
+$(".fa-plus").click(function () {
+    $("input[type='text'").fadeToggle();
 });
